7101fef37513996d1c10bca083eb1e64ed4d7614
SerhiiDziuba1309 <168010375+SerhiiDziuba1309@users.noreply.github.com>
2024-07-20 11:51:52 +0200

    task-5

---
+++ js/task-5.js
@@ -0,0 +1,16 @@
+// Напишіть функцію, яка приймає рядок як аргумент
+// і повертає новий рядок, в якому регістр кожного символу
+//інвертовано(великі літери стають маленькими, а маленькі – великими)
+function invertCase(str) {
+  let invertString = "";
+  for (const letter of str) {
+    if (letter.toUpperCase() === letter) {
+      invertString += letter.toLowerCase();
+    } else {
+      invertString += letter.toUpperCase();
+    }
+  }
+  return invertString;
+}
+console.log(invertCase("Hello World"));
+console.log(invertCase("helLO woRld"));
