dc13444cd9dc5dc0671cae1a7ca16da23b0ac732
Artur Luniaka <artur01049718@gmail.com>
2024-09-14 09:05:34 +0100

    task-22

---
+++ js/task-22.js
@@ -0,0 +1,38 @@
+// Додай функціонал зміни теми при натисканні(подія change) на чекбокс
+// #theme-switch-toggle у тулбарі.
+// За замовчуванням тема світла.
+// При зміні теми необхідно додавати на елемент body клас light-theme або dark-theme.
+// Вибрана тема повинна зберігатись між перезавантаженнями сторінки. 
+// Для зберігання теми використовуй localStorage.
+// Якщо при завантаженні сторінки тема темна, не забудь поставити властивість checked 
+// у чекбоксу #theme -switch-toggle у true, щоб повзунок зрушив у правильне положення.
+// Для зручності зберігання списку використовуй такий перелік.
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const themeInput = document.querySelector('.theme-switch__toggle');
+const bodyColor = document.querySelector('body');
+
+const handleColor = (event) => {
+    event.currentTarget;
+    if (event.currentTarget.checked) {
+        bodyColor.classList.remove('light-theme');
+        bodyColor.classList.add(Theme.DARK);
+        localStorage.setItem('theme', Theme.DARK)
+    } else {
+        bodyColor.classList.remove(Theme.DARK);
+        bodyColor.classList.add('light-theme');
+        localStorage.setItem('theme', 'light-theme')
+    }
+
+
+};
+
+themeInput.addEventListener('change', handleColor);
+
+
+
+
